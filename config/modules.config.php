a00d8cc8d9e8b072c228ef00b51cd452a9c5e84b
Thiago da Silva <44154599+thiagoSLV@users.noreply.github.com>
2022-12-12 12:57:29 -0300

    Adicionado container composer

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,7 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Hydrator',
     'Laminas\Mvc\I18n',
     'Laminas\I18n',
     'Laminas\InputFilter',
