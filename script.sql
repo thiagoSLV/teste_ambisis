4a0593788c6dc80cbafca608b6678425170fa838
Thiago da Silva <thiago.d.silv@gmail.com>
2022-12-12 18:22:40 -0300

    Adicionado script para criação da base de dados

--- script.sql
+++ script.sql
@@ -1,22 +1,25 @@
-CREATE database teste_ambisis;
+CREATE DATABASE IF NOT EXISTS teste_ambisis;
 use teste_ambisis;
-CREATE table empresa(
-    id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    razao_social(30),
-    cnpj int(14),
-    cep int(8),
-    cidade varchar(30),
-    estado varchar(2),
-    bairro varchar(30),
-    complemento varchar(30)
-);
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(14) DEFAULT NULL,
+  `cep` varchar(8) DEFAULT NULL,
+  `cidade` varchar(30) DEFAULT NULL,
+  `estado` varchar(2) DEFAULT NULL,
+  `bairro` varchar(30) DEFAULT NULL,
+  `complemento` varchar(30) DEFAULT NULL,
+  `razao_social` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE table licenca(
-    id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    empresa_id int,
-    numero varchar(30),
-    orgao_ambiental varchar(30),
-    emissao date,
-    validade date,
-    CONSTRAINT tb_fk FOREIGN KEY (empresa_id) REFERENCES empresa(id)
-);
+CREATE TABLE IF NOT EXISTS `licenca` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `empresa_id` int DEFAULT NULL,
+  `numero` varchar(30) DEFAULT NULL,
+  `orgao_ambiental` varchar(30) DEFAULT NULL,
+  `emissao` date DEFAULT NULL,
+  `validade` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tb_fk` (`empresa_id`),
+  CONSTRAINT `tb_fk` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
